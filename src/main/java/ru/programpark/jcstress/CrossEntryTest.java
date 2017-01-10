2a1b45db25acbba6a355092fadad0840ce8b9a35
vkozyr <kozyr@programpark.ru>
2017-01-10 19:25:10 +0300

    test changes bitset for entry

--- src/main/java/ru/programpark/jcstress/CrossEntryTest.java
+++ src/main/java/ru/programpark/jcstress/CrossEntryTest.java
@@ -12,21 +12,7 @@ import org.openjdk.jcstress.infra.results.LongResult4;
 @Outcome(expect = Expect.FORBIDDEN, desc = "other values")
 public class CrossEntryTest {
     public final static int SIZE = 256;
-    public final static int MIDDLE = 256/2-1;
-
-    @State
-    public static class UnsyncEntry {
-        final Object[] values = new Object[SIZE];
-
-        public Object get(int idx) {
-            return values[idx];
-        }
-
-        public void set(int idx, Object o) {
-            values[idx] = o;
-        }
-    }
-
+    public final static int MIDDLE = SIZE / 2 - 1;
 
     @Actor
     public void actor1(UnsyncEntry myState, LongResult4 res) {
@@ -40,8 +26,6 @@ public class CrossEntryTest {
         res.r2 = (long) myState.get(1);
     }
 
-
-
     @Actor
     public void actor3(UnsyncEntry myState, LongResult4 res) {
         myState.set(MIDDLE, -2L);
@@ -52,6 +36,19 @@ public class CrossEntryTest {
     public void actor4(UnsyncEntry myState, LongResult4 res) {
         myState.set(SIZE - 1, -1L);
         res.r4 = (long) myState.get(SIZE - 1);
+    }
+
+    @State
+    public static class UnsyncEntry {
+        final Object[] values = new Object[SIZE];
+
+        public Object get(int idx) {
+            return values[idx];
+        }
+
+        public void set(int idx, Object o) {
+            values[idx] = o;
+        }
     }
 
 
--- src/main/java/ru/programpark/jcstress/EntryChangesTest.java
+++ src/main/java/ru/programpark/jcstress/EntryChangesTest.java
@@ -8,11 +8,10 @@ import org.openjdk.jcstress.infra.results.LongResult4;
  * Created by kozyr on 10.01.2017.
  */
 @JCStressTest
-@Outcome(id = "1, 2, -2, -1", expect = Expect.ACCEPTABLE, desc = "all set visible")
+@Outcome(id = "1, 2, -1, 7", expect = Expect.ACCEPTABLE, desc = "all set visible")
 @Outcome(expect = Expect.FORBIDDEN, desc = "other values")
-public class EntryTest {
-    public final static int SIZE = 256;
-    public final static int MIDDLE = SIZE / 2 - 1;
+public class EntryChangesTest {
+    public final static int SIZE = 64;
 
     @Actor
     public void actor1(UnsyncEntry myState) {
@@ -25,33 +24,30 @@ public class EntryTest {
     }
 
     @Actor
-    public void actor3(UnsyncEntry myState) {
-        myState.set(MIDDLE, -2L);
-    }
-
-    @Actor
     public void actor4(UnsyncEntry myState) {
-        myState.set(SIZE - 1, -1L);
+        myState.set(2, -1L);
     }
 
     @Arbiter
     public void arbiter(UnsyncEntry myState, LongResult4 res) {
         res.r1 = (long) myState.get(0);
         res.r2 = (long) myState.get(1);
-        res.r3 = (long) myState.get(MIDDLE);
-        res.r4 = (long) myState.get(SIZE - 1);
+        res.r3 = (long) myState.get(2);
+        res.r4 = myState.changes;
     }
 
     @State
     public static class UnsyncEntry {
         final Object[] values = new Object[SIZE];
+        long changes = 0;
 
         public Object get(int idx) {
             return values[idx];
         }
 
-        public void set(int idx, Object o) {
+        public synchronized void set(int idx, Object o) {
             values[idx] = o;
+            changes |= (1 << idx);
         }
     }
 
